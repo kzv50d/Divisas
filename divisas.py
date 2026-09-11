a1efd5f01b5ab231a6bb881598f8e90078ea8dfc
kzv50d <195387848+kzv50d@users.noreply.github.com>
2026-09-10 18:35:51 -0600

    Add files via upload

--- divisas.py
+++ divisas.py
@@ -2,6 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+script_code = """import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
 url = "https://yahoo.com"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
@@ -33,11 +37,10 @@ if tabla:
                 "Precio Actual": precio_actual
             })
 
-    if datos:
-        df = pd.DataFrame(datos)
-        df.to_csv("catalogo_divisas.csv", index=False, encoding="utf-8-sig")
-        print(f"¡Scraping exitoso! Se procesaron {len(df)} divisas y se guardaron en 'catalogo_divisas.csv'.")
-    else:
-        print("Error: No se lograron extraer filas con datos válidos de la tabla.")
-else:
-    print("Error: No se localizó ninguna estructura de tabla (<table>) en el código HTML.")
+    df = pd.DataFrame(datos)
+    df.to_csv("catalogo_divisas.csv", index=False, encoding="utf-8-sig")
+   print(f"¡Scraping exitoso! Se procesaron {len(df)} divisas y se guardaron en 'catalogo_divisas.csv'.")
+  """
+
+with open("divisas.py", "w", encoding="utf-8") as f:
+    f.write(script_code)
